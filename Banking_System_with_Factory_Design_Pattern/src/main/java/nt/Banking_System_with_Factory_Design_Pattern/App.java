da1e50e98815622154cafe03ba4cdb7b43298cd0
Nitishroy1 <nitishk9199@gmail.com>
2026-03-09 23:29:17 +0530

    Banking system

---
+++ Banking_System_with_Factory_Design_Pattern/src/main/java/nt/Banking_System_with_Factory_Design_Pattern/App.java
@@ -0,0 +1,10 @@
+package nt.Banking_System_with_Factory_Design_Pattern;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
